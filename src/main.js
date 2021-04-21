7cbb60793db8f33eb0e39e6f348539bc23b6c568
zwzwzw2019 <1517388906@qq.com>
2021-04-21 13:26:37 +0800

    book03

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import Vue from 'vue'
 import App from './App.vue'
 
@@ -6,3 +7,9 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+axios({
+  url:"http://123.207.32.32:8000/home/multidata"
+}).then(res =>{
+  console.log(res);
+})
